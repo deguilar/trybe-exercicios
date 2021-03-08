d342bb1173f47a109b6f509652af5080b6901d3e
André Aguilar <andreaguilar.ti@gmail.com>
2021-03-08 18:39:53 -0300

    exercicio 2_4

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio_2_4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio_2_4.js
@@ -1,4 +1,4 @@
-// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
 function maiorPalavra(palavras) {
     let maiorPalavra = palavras[0];
